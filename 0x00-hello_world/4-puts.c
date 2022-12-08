2054bb21ad2ec469fbfef84965a5156bf044b61c
Lesmutanga <lesmutanga@gmail.com>
2022-12-08 08:20:43 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main (void)
+{
+	puts("\"puts ("\"Programming is like building a multilingual puzzle");
+			return(0);
+
