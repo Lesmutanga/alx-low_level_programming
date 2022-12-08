a6cb62b74b3692faae749893b7f473ce3228f6a2
Lesmutanga <lesmutanga@gmail.com>
2022-12-08 08:57:14 -0800

    task 5 all

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * Main - Entry point
- * Return: Always 0 (success)
+ * main - Entry point
+ * Return: Always 0
  */
 int main(void)
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
